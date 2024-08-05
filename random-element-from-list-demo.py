b70f84f73f13f4084bd27063f502e7fb0dc144d5
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-05 07:19:27 -0400

    Create random-element-from-list-demo.py

---
+++ random-element-from-list-demo.py
@@ -0,0 +1,15 @@
+# Abe Lincoln
+# 28 AUG 20XX
+# Select Random Element from List
+
+# Import the random module first
+import random
+
+# Define your list
+movie_list = ['The Godfather', 'The Wizard of Oz', 'Top Gun', 'The Shawshank Redemption', 'Pulp Fiction']
+
+# Select a movie at random from list
+movie = random.choice(movie_list)
+
+# Display the randomly selected movie title
+print(movie)
